6c28b274a82407ba22ca3b38cded52b7a326fb57
sogeuni <sogeuni@gmail.com>
2018-12-24 14:44:54 +0900

    tv info parsing 루틴 변경

--- util.py
+++ util.py
@@ -41,20 +41,29 @@ def get_program_info(title):
     soup = BeautifulSoup(req.text, "html.parser")
 
     try:
-      i = soup.find('div', class_='info_tv')
+      info_tv = soup.find('div', class_='info_tv')
 
-      # logger.debug(i.prettify())
+      logger.debug(info_tv.prettify())
 
-      title = i.select_one('.txt_subject').text.strip()
-      genre = i.find('dt', string=re.compile(u'^(장르|정보)$')).find_next_sibling('dd').text.split(',')[0].split('|')[0].strip()
-      year = '' # TODO
-      bb = i.find('dt', string="방영시간 정보").find_next_sibling('dd').select('.txt_info')
+      title = info_tv.select_one('.txt_subject').text.strip()
+      i = info_tv.find('dt', string=re.compile(u'^정보$'))
 
-      logger.debug(bb[0].text.strip())
-      logger.debug(bb[1].text.strip())
-      logger.debug(bb[2].text.strip())
+      if i is not None:
+        logger.info('case 1')
+        i = i.find_next_sibling('dd').text.split('|')
+        genre = i[0].strip()
+        year = i[1].split('.')[0].strip()
+        i = info_tv.find('dt', string="방영시간 정보").find_next_sibling('dd').select('.txt_info')
+        channel = i[0].text.strip()
+      else:
+        logger.info('case 2')
+        i = info_tv.find('dt', string=re.compile(u'^장르$'))
+        genre = i.find_next_sibling('dd').text.split(',')[0].strip()
+        i = info_tv.find('dt', string="방영시간 정보").find_next_sibling('dd').select('.txt_info')
+        channel = i[0].text.strip()
+        year = i[2].text.split('.')[0].strip()
 
-      info = {'title': title, 'genre': genre, 'year': year}
+      info = {'title': title, 'channel': channel, 'genre': genre, 'year': year}
     except:
       logger.debug('parsing error:')
       logger.error(sys.exc_info())
